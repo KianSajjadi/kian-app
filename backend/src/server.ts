8102f317efa2380aa8d02119277d8460577a6123
KianSajjadi <kian.s.sajjadi@gmail.com>
2021-03-16 16:27:53 +1100

    Added User table Changed ormconfig and tsconfig

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
+import "reflect-metadata";
 import express from "express";
 import router from "./routes/router"
+import {createConnection} from "typeorm";
 import cors from "cors";
+
 const app = express();
+
+createConnection().then(async connection => {
+}).catch(error => console.log(error));
 
 app.use(cors());
 app.use("/", router);
@@ -9,4 +15,4 @@ app.use("/test", router);
 
 app.listen(8000, () => {
     console.log("App listening at http://localhost:8000");
-}); 
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { apiResponse: "" };
-}
+	}	
 
-callAPI() {
-    fetch("http://localhost:8000/test")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-}
+	callAPI() {
+	    fetch("http://localhost:8000/test")
+	        .then(res => res.text())
+	        .then(res => this.setState({ apiResponse: res }));
+	}
 
-componentWillMount() {
-    this.callAPI();
-}
+	componentWillMount() {
+	    this.callAPI();
+	}
 
   render() {
     return (
